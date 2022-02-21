43ca6f902e2ad934b6b90ca4c0d5b6c67a345594
Daniel <D.rivera12394@gmail.com>
2022-02-21 17:02:43 -0500

    commit tree 8ada045ebce27f0ec0583eb4986eb0adbf32e75d

---
+++ db/migrate/20220221220028_create_create_cheeses.rb
@@ -0,0 +1,11 @@
+class CreateCreateCheeses < ActiveRecord::Migration[6.1]
+  def change
+    create_table :create_cheeses do |t|
+      t.string :first_name
+      t.string :last_name
+      t.integer :grade
+
+      t.timestamps
+    end
+  end
+end
